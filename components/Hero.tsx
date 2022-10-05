c008a7a586479716d98f192cd1d4d698b144ee2d
JordonP123 <jordonaren@gmail.com>
2022-10-05 08:10:58 -0400

    mucho progress

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,7 @@ import BackgroundCircles from './BackgroundCircles'
 import photoOfMe from '../photos/me.jpg'
 import Image from 'next/image'
 import { relative } from 'path'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -27,12 +28,28 @@ export default function Hero({ }: Props) {
                 src={photoOfMe}
                 alt='photo of user'
             />
-            <div>
-                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[12px]'>Software Engineer</h2>
-                <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+            <div className='z-20'>
+                <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[12px]'>
+                    Software Engineer
+                </h2>
+                <h1 className='text-5xl font-semibold px-10'>
                     <span className='ml-5'>{text}</span>
                     <Cursor cursorColor='#c52222' />
                 </h1>
+                <div className='pt-5'>
+                    <Link href='#about'>
+                        <button className='heroButton'>About</button>
+                    </Link>
+                    <Link href='#experience'>
+                        <button className='heroButton'>Experience</button>
+                    </Link>
+                    <Link href='#skills'>
+                        <button className='heroButton'>Skills</button>
+                    </Link>
+                    <Link href='#projects'>
+                    <button className='heroButton'>Projects</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,44 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import About from '../components/About'
+import Experience from '../components/Experience'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Skills from '../components/Skills'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen 
-    snap-y snap-mandatory overflow-scroll z-0">
+    snap-y snap-mandatory overflow-scroll z-0 scroll-smooth">
       <Head>
         <title>Jordons Portfolio</title>
       </Head>
 
-    <Header/>
+      <Header />
 
-    {/*  Hero */}
-    <section id='hero' className='snap-center'>
-      <Hero />
-    </section>
+      <section id='hero' className='snap-start'>
+        <Hero />
+      </section>
 
-    {/*  About */}
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
 
-    {/*  Experience */}
+      <section id='experience' className='snap-center'>
+        <Experience />
+      </section>
 
-    {/*  Skills */}
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
 
-    {/*  Contact Me */}
+      {/*  Contact Me */}
 
 
 
 
-      </div>
+    </div>
   )
 }
 
