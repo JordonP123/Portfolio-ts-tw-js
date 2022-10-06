5319c2cd109ad3248a11c029add52f3eb1b699d5
JordonP123 <jordonaren@gmail.com>
2022-10-06 11:20:17 -0400

    yet another error

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
     scrollbar-track-gray-400/20 scrollbar-thumb-[#c52222]/80">
       <Head>
         <title>Jordons Portfolio</title> 
-        <script src="https://kit.fontawesome.com/2e3bd962e7.js" crossorigin="anonymous"></script>{/*eslint-disable-line*/}
+        <script src="https://kit.fontawesome.com/2e3bd962e7.js"></script>{/*eslint-disable-line*/}
       </Head>
 
       <Header />
